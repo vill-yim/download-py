b04f17ee1cb739bceba07c09f8e67d54c4dd35d9
yimco <yimco@gmail.com>
2024-11-11 01:06:50 -0500

    acplicación para descargar de youtube y guardarlo donde gustes

--- download.py
+++ download.py
@@ -1,44 +1,40 @@
 import yt_dlp
 import os
 
+def main():
+        url = input("Ingrese una url:\n")
+        preference = input("Prefieres mp3 o mp4?\n")
+        rute_option = input(f"La ruta por defecto es {os.getcwd()}, ¿Desea cambiarla? y/n \n")
+        if rute_option == 'y':
+            rute = input("Introduce la nueva ruta: ").strip()
+        if rute_option == 'n':
+            rute = os.getcwd()
 
 
-def downloadMedia():
-    url = input("Ingrese una url:\n")
-    preference = input("Prefieres mp3 o mp4?\n")
+        if preference == "mp3":
+            ydl_opts = {
+                "format": "bestaudio/best",
+               "outtmpl": f"{rute}/%(title)s.%(ext)s",
+                "postprocessors": [
+                    {
+                        "key": "FFmpegExtractAudio",
+                        "preferredcodec": preference,
+                        "preferredquality": "192",
+                    }
+                ],
+                "playlist": False,
+                "playlist_items": "1",
+            }
 
-    if preference == "mp4":
-        ydl_opts = {}
-
-    if preference == "mp3":
-        ydl_opts = {
-            "format": "bestaudio/best",
-            "outtmpl": "%(title)s.%(ext)s",
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": preference,
-                    "preferredquality": "192",
-                }
-            
-            ],
-            "playlist":False,
-            "playlist_items":"1"
-        }
-        
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-    if preference == "mp4":
-            ydl_opts.update({
-                "format": "best[ext=mp4]"
-            })
-
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-            print(f"{preference} descargado exitosamente")
-    except Exception as e:
+        if preference == "mp4":
+            ydl_opts.update({"format": "best[ext=mp4]"})
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+                print(f"{preference} descargado exitosamente")
+        except Exception as e:
             print(f"Error durante la descarga: {str(e)}")
+
+
 if __name__ == "__main__":
-    downloadMedia()
+    main()
